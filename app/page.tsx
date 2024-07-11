2ea44424d578092c06bd90d08d2b374148dcf6d9
Vhkan <vhkan@yahoo.com>
2024-07-11 12:39:58 -0600

    Add shadcn-ui library

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
 
 export default function Home() {
   return (
     <div>
       <h1 className="text-white">Home</h1>
+      <Button>Click me</Button>
     </div>
   );
 }
