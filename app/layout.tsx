a876412a89d9f8fe477c3bd250c88e5851f55b4e
Vhkan <vhkan@yahoo.com>
2024-07-11 12:59:32 -0600

    add themeprovider, wrap layout.tsx component into it

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,12 @@ import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/theme-provide";
 
-const fontSans = Plus_Jakarta_Sans({ 
+const fontSans = Plus_Jakarta_Sans({
   subsets: ["latin"],
   weight: ['300', '400', '500', '600', '700'],
-  variable: '--font-sans' 
+  variable: '--font-sans'
 });
 
 export const metadata: Metadata = {
@@ -22,7 +23,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       {/* cn for classname, min-h-screen(min height of 100vh) */}
-      <body className={cn('min-h-screen-dvh bg-dark-300 font-sans antialiased', fontSans.variable)}>{children}</body> 
+      <body className={cn('min-h-screen-dvh bg-dark-300 font-sans antialiased', fontSans.variable)}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
