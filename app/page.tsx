66cf761dee4d3fb504fdd8ea4b90969f34862179
Vhkan <vhkan@yahoo.com>
2024-07-15 23:37:54 -0600

    Create and add logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/button";
+import { PatientForm } from "@/components/forms/PatientForm";
 import Image from "next/image";
 
 
@@ -7,7 +7,12 @@ export default function Home() {
     <div className="flex h-screen max-h-screen">
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
-          <Image />
+          <Image src='/assets/icons/medicare-high-resolution-logo-transparent.png'
+          height={1000}
+          width={1000}
+          alt="patient"
+          className="mb-12 h-10 w-fit" />
+          <PatientForm/>
         </div>
       </section>
     </div>
