461696567c07805ac1d9d81f52ee220179b8fec8
Vhkan <vhkan@yahoo.com>
2024-08-20 20:50:12 -0600

    Add register component

--- app/patients/[userId]/register/page.tsx
+++ app/patients/[userId]/register/page.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';  
 
 const Register = () => {
   return (
-    <div>Register</div>
-  )
+    <div className='flex h-screen'>
+      <section className='overflow-y-auto w-full'>
+        <div className='max-w-[860px] flex flex-col py-10 mx-auto'>
+          <Image 
+            src='/assets/icons/medicare-high-resolution-logo-transparent.png' 
+            height={1000}
+            width={1000}
+            alt='Medicare Logo'
+            className='mb-12 h-10 w-fit'
+          />
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default Register;
--- lib/appwrite.config.ts
+++ lib/appwrite.config.ts
@@ -11,16 +11,26 @@ type AppwriteConfig = {
   BUCKET_ID: string;
 }
 
-const {
-  PROJECT_ID, API_KEY, DATABASE_ID, PATIENT_COLLECTION_ID, DOCTOR_COLLECTION_ID, APPOINTMENT_COLLECTION_ID, BUCKET_ID, ENDPOINT
-} = process.env as unknown as AppwriteConfig;
+export const {
+  NEXT_PUBLIC_ENDPOINT: ENDPOINT,
+  PROJECT_ID,
+  API_KEY,
+  DATABASE_ID,
+  PATIENT_COLLECTION_ID,
+  DOCTOR_COLLECTION_ID,
+  APPOINTMENT_COLLECTION_ID,
+  NEXT_PUBLIC_BUCKET_ID: BUCKET_ID,
+} = process.env;
 
 const client = new sdk.Client();
 
 client
-  .setEndpoint(ENDPOINT!)
-  .setProject(PROJECT_ID!)
-  .setKey(API_KEY!);
+  // .setEndpoint(ENDPOINT!)
+  // .setProject(PROJECT_ID!)
+  // .setKey(API_KEY!);
+  .setEndpoint("https://cloud.appwrite.io/v1")
+  .setProject("66b17e020026edd8eb79")
+  .setKey("92b45c624295e13194f2d402473fe28d445eaa0080d4ff9065025c9e45669060bf52463bb7f62223f6f3768b720f5dc300d423fba7de3a3e45fecdb13602d29e2562b83880dd5007c5e2a4c4bb4fce5fcea4945aa7d8c2a352fa19b5f61911560209afb1d5c54d9bb66a461dbe56e5d16662401560097c71606abd59d1b5b9d3");
 
 
 export const databases = new sdk.Databases(client);
