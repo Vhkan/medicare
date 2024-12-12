7978b8acd68b4b9eababa53350990aaf014508c0
Vhkan <vhkan@yahoo.com>
2024-12-11 22:34:16 -0700

    debug Register form, add new appointment starting page

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -12,7 +12,6 @@ import {
 } from "../appwrite.config";
 import { parseStringify } from "../utils";
 import { ID, Query } from "node-appwrite";
-import { InputFile } from "node-appwrite";
 
 export const createUser = async (user: CreateUserParams) => {
   try {
@@ -25,45 +24,41 @@ export const createUser = async (user: CreateUserParams) => {
     );
 
     return parseStringify(newUser);
-    
   } catch (error: any) {
-    if (error && error?.code === 409) {
+    if (error?.code === 409) {
       const documents = await users.list([
-        Query.equal('email', [user.email])
-      ])
-      return documents?.users[0]
+        Query.equal("email", [user.email]),
+      ]);
+      return documents?.users[0];
     }
   }
-};  
+};
 
-//Get user server action
 export const getUser = async (userId: string) => {
   try {
     const user = await users.get(userId);
     return parseStringify(user);
   } catch (error) {
-    console.log(error);
+    console.error("Error fetching user:", error);
   }
 };
 
-//register patient
 export const registerPatient = async ({
   identificationDocument,
   ...patient
 }: RegisterUserParams) => {
   try {
     let file;
+
     if (
       identificationDocument &&
       identificationDocument.get("blobFile") &&
       identificationDocument.get("fileName")
     ) {
-      const inputFile = InputFile.fromBlob(
-        identificationDocument.get("blobFile") as Blob,
-        identificationDocument.get("fileName") as string
-      );
+      const blobFile = identificationDocument.get("blobFile") as Blob;
+      const fileName = identificationDocument.get("fileName") as string;
 
-      file = await storage.createFile(BUCKET_ID!, ID.unique(), inputFile);
+      file = await uploadFile(blobFile, fileName);
     }
 
     const newPatient = await databases.createDocument(
@@ -82,5 +77,18 @@ export const registerPatient = async ({
     return parseStringify(newPatient);
   } catch (error) {
     console.error("An error occurred while creating a new patient:", error);
+  }
+};
+
+const uploadFile = async (blob: Blob, fileName: string) => {
+  try {
+    // Convert Blob to File
+    const file = new File([blob], fileName, { type: blob.type });
+
+    // Upload the File to Appwrite Storage
+    return await storage.createFile(BUCKET_ID!, ID.unique(), file);
+  } catch (error) {
+    console.error("File upload failed:", error);
+    throw error;
   }
 };
