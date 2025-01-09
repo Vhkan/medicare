935f5b5d62760900ce9a48e20388f79927fcadc0
Vhkan <vhkan@yahoo.com>
2025-01-08 21:49:47 -0700

    add get patient action fucntion

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -43,6 +43,22 @@ export const getUser = async (userId: string) => {
   }
 };
 
+export const getPatient = async (userId: string) => {
+  try {
+    const patient = await databases.listDocuments(
+      DATABASE_ID!,
+      PATIENT_COLLECTION_ID!,
+      [
+        Query.equal("$userId", userId),
+        Query.limit(1),
+      ]
+    );
+    return parseStringify(patient.documents[0]);
+  } catch (error) {
+    console.error("Error fetching user:", error);
+  }
+};
+
 export const registerPatient = async ({
   identificationDocument,
   ...patient
